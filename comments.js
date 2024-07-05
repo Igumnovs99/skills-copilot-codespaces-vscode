4dd4214c610d2eb2e047b3aa320fbd751fb674b6
Igumnovs99 <deniss.igumnovs@datalytyx.com>
2024-07-05 15:31:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -11,3 +11,19 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+// Create a route for comments
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page');
+});
+// Create a route for members
+app.get('/members', (req, res) => {
+    res.send('This is the members page');
+});
+// Create a route for skills
+app.get('/skills', (req, res) => {
+    res.send('This is the skills page');
+});
+// Create a route for about
+app.get('/about', (req, res) => {
+    res.send('This is the about page');
+});
