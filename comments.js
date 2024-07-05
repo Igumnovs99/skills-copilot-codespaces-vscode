fab36633a1e57b34b5ebbfd50df742c9f1905a0c
Igumnovs99 <deniss.igumnovs@datalytyx.com>
2024-07-05 15:38:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,29 +1,63 @@
 // Create web server
-const express = require('express');
-const app = express();
-// Create a port
-const PORT = 5000;
-// Create a route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-// Create a server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-// Create a route for comments
-app.get('/comments', (req, res) => {
-    res.send('This is the comments page');
-});
-// Create a route for members
-app.get('/members', (req, res) => {
-    res.send('This is the members page');
-});
-// Create a route for skills
-app.get('/skills', (req, res) => {
-    res.send('This is the skills page');
-});
-// Create a route for about
-app.get('/about', (req, res) => {
-    res.send('This is the about page');
-});
+// 1. Load required modules
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var url = require("url");
+var qs = require("querystring");
+var comments = [];
+
+// 2. Create server
+http.createServer(function(request, response) {
+    // Parse the request containing file name
+    var pathname = url.parse(request.url).pathname;
+    // Print the name of the file for which request is made.
+    console.log("Request for " + pathname + " received.");
+    // Read the requested file content from file system
+    if (pathname === "/") {
+        pathname = "/index.html";
+    }
+    if (pathname === "/index.html") {
+        fs.readFile(pathname.substr(1), function(err, data) {
+            if (err) {
+                console.log(err);
+                response.writeHead(404, { "Content-Type": "text/html" });
+            } else {
+                response.writeHead(200, { "Content-Type": "text/html" });
+                response.write(data.toString());
+            }
+            response.end();
+        });
+    } else if (pathname === "/comments") {
+        if (request.method === "POST") {
+            var body = "";
+            request.on("data", function(data) {
+                body += data;
+            });
+            request.on("end", function() {
+                body = qs.parse(body);
+                comments.push(body.comment);
+                response.writeHead(200, { "Content-Type": "text/plain" });
+                response.end();
+            });
+        } else {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.write(JSON.stringify(comments));
+            response.end();
+        }
+    } else {
+        fs.readFile(pathname.substr(1), function(err, data) {
+            if (err) {
+                console.log(err);
+                response.writeHead(404, { "Content-Type": "text/html" });
+            } else {
+                response.writeHead(200, { "Content-Type": "text/html" });
+                response.write(data.toString());
+            }
+            response.end();
+        });
+    }
+}).listen(3000);
+
+// Print URL for accessing server
+console.log("Server running at http://
